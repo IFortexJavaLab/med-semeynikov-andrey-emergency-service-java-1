b93d1e57930e81799c324768338f41c15fa3567d
AndreySemeynikovIF <andrey.semeynikov@ifortex.com>
2025-04-07 11:32:10 +0300

    MED-54: Get name while updating geo

--- src/main/java/com/ifortex/internship/emergencyservice/service/ParamedicLocationService.java
+++ src/main/java/com/ifortex/internship/emergencyservice/service/ParamedicLocationService.java
@@ -1,14 +1,20 @@
 package com.ifortex.internship.emergencyservice.service;
 
+import com.ifortex.internship.accountingapi.AccountingServiceApi;
+import com.ifortex.internship.accountingapi.dto.response.AccountDto;
+import com.ifortex.internship.accountingapi.exception.CustomFeignException;
 import com.ifortex.internship.emergencyservice.dto.request.UpdateParamedicLocationRequest;
 import com.ifortex.internship.emergencyservice.model.ParamedicLocation;
 import com.ifortex.internship.emergencyservice.repository.ParamedicLocationRepository;
+import com.ifortex.internship.emergencyservice.util.AccountDtoMapper;
+import com.ifortex.internship.medstarter.exception.custom.InternalServiceException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -17,11 +23,12 @@ import java.util.UUID;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ParamedicLocationService {
 
+    AccountDtoMapper accountDtoMapper;
+    AccountingServiceApi accountingServiceApi;
     ParamedicLocationRepository locationRepository;
 
     public void updateLocation(UpdateParamedicLocationRequest request, UUID paramedicId) {
-
-        //todo there can call to accounting to get medic's profile and get name
+        var accountDto = getAccountDtoFromAccountingService(paramedicId);
 
         var locationOpt = locationRepository.findById(paramedicId);
         ParamedicLocation location;
@@ -29,16 +36,33 @@ public class ParamedicLocationService {
             location = locationOpt.get();
             location.setLongitude(request.longitude());
             location.setLatitude(request.latitude());
+            location.setParamedicFirstName(accountDto.getFirstName());
         } else {
             location = ParamedicLocation.builder()
                 .paramedicId(paramedicId)
                 .latitude(request.latitude())
                 .longitude(request.longitude())
+                .paramedicFirstName(accountDto.getFirstName())
                 .build();
         }
 
         locationRepository.save(location);
         log.info("Updated location for paramedic [{}]: lat={}, lng={}",
             paramedicId, request.latitude(), request.longitude());
+    }
+
+    private AccountDto getAccountDtoFromAccountingService(UUID paramedicId) {
+        AccountDto clientAccount;
+        try {
+            log.info("Send request to accounting service to get medics profile with account ID: {}", paramedicId);
+            var accountDtoResponseEntity = accountingServiceApi.getUserProfileByAuthentication();
+            clientAccount = accountDtoMapper.map((Map<String, Object>) accountDtoResponseEntity.getBody());
+            log.info("Got user profile with account ID: {} from accounting service", paramedicId);
+        } catch (CustomFeignException ex) {
+            log.error("Error occurred during call to the auth service");
+            throw new InternalServiceException("Something went wrong, try later");
+        }
+        assert clientAccount != null;
+        return clientAccount;
     }
 }
